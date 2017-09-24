0dc8012bd508d7512cec97c8af5028d863c6de20
Ryan Murphy <ryan.murphy@alumni.stanford.edu>
2017-09-24 16:56:29 -0700

    finish creating node server and configuring for github api requests

--- animal_fun.js
+++ animal_fun.js
@@ -1,9 +1,86 @@
 const fs = require('fs');
-
-fs.readFile('./animals.txt', 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(data);
-});
+const http = require('http');
+const qs = require('querystring');
+const cache = {};
+//
+// // fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+// //   if (err) {
+// //     console.log(err);
+// //     return;
+// //   }
+// //   console.log(data);
+// // });
+// //
+// // fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
+// //   if (err) {
+// //     console.log(err);
+// //     return;
+// //   }
+// //   console.log('file successfully written');
+// // });
+//
+// function selectAnimals(animalString, animalLetter) {
+//   return animalString
+//     .split('\n')
+//     .filter(animal => animal.startsWith(animalLetter))
+//     .join('\n');
+// }
+//
+// const animalLetter = process.argv[2].toUpperCase();
+//
+//
+// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//
+//   const animals = selectAnimals(data, animalLetter);
+//
+//   fs.writeFile(`${animalLetter}_animals.txt`, animals, err => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log(`successfully created ${animalLetter}_animals.txt`);
+//   });
+// });
+//
+// const animalServer = http.createServer((req, res) => {
+//   const query = req.url.split('?')[1];
+//   if (query !== undefined) {
+//     const animalLetter = qs.parse(query).letter.toUpperCase();
+//
+//     if (cache[animalLetter] !== undefined) {
+//       res.end(cache[animalLetter]);
+//     }
+//
+//     if (animalLetter !== undefined) {
+//       fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+//         if (err) {
+//           console.log(err);
+//           res.end('IT WENT POORLY');
+//           return;
+//         }
+//         const animals = selectAnimals(data, animalLetter);
+//         cache[animalLetter] = animals;
+//         res.end(animals);
+//       });
+//     }
+//   } else {
+//     if (cache['animals'] !== undefined) {
+//       res.end(cache['animals']);
+//     }
+//     fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+//       if (err) {
+//         console.log(err);
+//         res.end('IT WENT POORLY');
+//         return;
+//       }
+//       cache['animals'] = data;
+//       res.end(data);
+//     });
+//   }
+// });
+//
+// animalServer.listen(8000, () => console.log("I'm listening on port 8000"));
